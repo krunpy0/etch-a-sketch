c70ddf206b42dde630e81ca5fe314943b8e0ed6c
krunpy <maksimglinin17984@gmail.com>
2025-06-12 21:23:46 +0500

    Make resizable grid and reset button

--- script.js
+++ script.js
@@ -1,19 +1,50 @@
-function createGrid() {
-  for (let i = 0; i < 16 * 16; i++) {
-    const content = document.querySelector('.content')
-    const square = document.createElement('div')
-    square.classList.add('square')
-    content.appendChild(square)
+function createGrid(size) {
+  const content = document.querySelector('.content');
+  content.innerHTML = '';
+  const squareSize = 800 / size;
+  console.log(squareSize)
 
+  for (let i = 0; i < size * size; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    content.appendChild(square);
+    square.style.width = `${squareSize}px`;
+    square.style.height = `${squareSize}px`;
   }
-}
-createGrid()
-
-
-
-const squares = document.querySelectorAll('.square')
-squares.forEach((square) => {
-  square.addEventListener('mouseover', () => {
-    square.classList.add('square-hover')
+  const squares = document.querySelectorAll('.square')
+  squares.forEach((square) => {
+    square.addEventListener('mouseover', () => {
+      console.log('yes')
+      square.classList.add('square-hover')
+    })
   })
+  const resetBtn = document.querySelector('.reset-btn')
+  console.log(resetBtn)
+  resetBtn.addEventListener('click', () => {
+    console.log('reset')
+    squares.forEach((square) => {
+      square.classList.remove('square-hover')
+    })
+  })
+}
+createGrid(16)
+
+
+
+
+
+
+let size;
+
+
+const sizeBtn = document.querySelector('.size-btn')
+
+sizeBtn.addEventListener('click', () => {
+  size = prompt('Enter the grid size (16-100)');
+  if (size < 16 || size > 100) {
+    alert('Please enter size from 16 to 100')
+  } else {
+    createGrid(size)
+  }
 })
+
